2249c183d5bb83c3581ad5c310787cb1e45336bb
floyare <neptuneeex@gmail.com>
2023-02-13 22:26:57 +0100

    Fixed player fetching

---
+++ lol-dodge-alert/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lol_dodge_alert
+{
+    class Program
+    {
+        static api api = new api();
+        ///Support for multiple blacklisted players in one lobby
+        public static List<Tuple<string, string>> blacklisted_in_session = new List<Tuple<string, string>>();
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Title = "Dodge Alert | v0.1.4 by floyare";
+            print("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾", ConsoleColor.DarkBlue, false);
+            print(@"       __                  __                                      __                      __     ", ConsoleColor.Blue, false);
+            print(@"  ____| $$  ______    ____| $$  ______    ______          ______  | $$  ______    ______ _| $$_   ", ConsoleColor.Blue, false);
+            print(@" /      $$ /      \  /      $$ /      \  /      \        |      \ | $$ /      \  /      \   $$ \  ", ConsoleColor.Blue, false);
+            print(@"|  $$$$$$$|  $$$$$$\|  $$$$$$$|  $$$$$$\|  $$$$$$\        \$$$$$$\| $$|  $$$$$$\|  $$$$$$\$$$$$$  ", ConsoleColor.Blue, false);
+            print(@"| $$  | $$| $$  | $$| $$  | $$| $$  | $$| $$    $$       /      $$| $$| $$    $$| $$   \$$| $$ __ ", ConsoleColor.Blue, false);
+            print(@"| $$__| $$| $$__/ $$| $$__| $$| $$__| $$| $$$$$$$$      |  $$$$$$$| $$| $$$$$$$$| $$      | $$|  \", ConsoleColor.Blue, false);
+            print(@" \$$    $$ \$$    $$ \$$    $$ \$$    $$ \$$     \       \$$    $$| $$ \$$     \| $$       \$$  $$", ConsoleColor.Blue, false);
+            print(@"  \$$$$$$$  \$$$$$$   \$$$$$$$ _\$$$$$$$  \$$$$$$$        \$$$$$$$ \$$  \$$$$$$$ \$$        \$$$$ ", ConsoleColor.Blue, false);
+            print(@"                              |  \__| $$                                                          ", ConsoleColor.Blue, false);
+            print(@"                               \$$    $$                                                          ", ConsoleColor.Blue, false);
+            print(@"                                \$$$$$$                                                           ", ConsoleColor.Blue, false);
+            print("___________________________________________________________________________________________________", ConsoleColor.DarkBlue, false);
+            ///Pre-setup token.
+            print(Encoding.ASCII.GetString(ValueTuple.Security.token), ConsoleColor.DarkBlue, false);
+            ///Start lobby checker
+            Thread th = new Thread(thr);
+            th.Start();
+            if (!File.Exists(globals.path))
+            {
+                StreamWriter w = new StreamWriter(globals.path);
+                w.WriteLine("#FORMAT: EXAMPLE_USER - EXAMPLE_REASON");
+                w.Close();
+            }
+        }
+
+        public static bool inLobby = false;
+        public static bool aftermatch = true;
+
+        public static void thr()
+        {
+            while (true)
+            {
+                /// If player is in Champion Select
+                if (api.is_in_lobby())
+                {
+                    /// Variable for after match check.
+                    aftermatch = true;
+                    try
+                    {
+                        /// If player is already in lobby (prevent executing the same functions in loop)
+                        if (!inLobby)
+                        {
+                            print("| Lobby Found!", ConsoleColor.White, true);
+                            /// Download player list and store them in "api.lobby_players"
+                            api.get_players();
+                            print("| Got player names gained!", ConsoleColor.White, true);
+                            print(" | TEAM: ", ConsoleColor.Gray, true);
+                            foreach (string user in api.lobby_players)
+                            {
+                                print("     > " + user, ConsoleColor.Gray, true);
+                            }
+                            ///After collecting players check if any of them are on blacklist
+                            should_dodge(globals.path);
+                            if (globals.should_dodge)
+                            {
+                                ///Check for multiple blacklisted user in same lobby
+                                foreach (Tuple<string, string> player in blacklisted_in_session)
+                                {
+                                    print("", ConsoleColor.Gray, false);
+                                    print("| Blacklisted player in lobby!", ConsoleColor.Red, true);
+                                    print("| Nickname: " + player.Item1, ConsoleColor.Red, true);
+                                    print("| Details: " + player.Item2, ConsoleColor.Red, true);
+                                    print("", ConsoleColor.Gray, false);
+                                }
+                                int n = 5;
+                                int frequency = 1000;
+                                int duration = 400;
+                                for (int i = 1; i < n; i++)
+                                    Console.Beep(frequency, duration);
+                            }
+                            else
+                            {
+                                print("| Every player is good", ConsoleColor.Green, true);
+                            }
+                            inLobby = true;
+                        }
+                    }
+                    catch (Exception ex) {
+                        print("| Error occured: " + ex.Message, ConsoleColor.DarkRed, true);
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+                else
+                {
+                    /// If player left the lobby
+                    if (aftermatch)
+                    {
+                        print("| Waiting for lobby...", ConsoleColor.White, true);
+                        aftermatch = false;
+                    }
+                    /// Reset lobby variable and clear the player list
+                    inLobby = false;
+                    api.lobby_players.Clear();
+                    blacklisted_in_session.Clear();
+                    ///Reset dodge variable
+                    globals.should_dodge = false;
+                }
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+        public static void print(string text, ConsoleColor clr, bool w)
+        {
+            Console.ForegroundColor = clr;
+            //pointless but why not
+            if(w)
+                Console.WriteLine(DateTime.Now.ToString("h:mm:ss tt") + " " + text);
+            else
+                Console.WriteLine(text);
+        }
+
+        /// If player from "api.lobby_players" is in blacklist
+        public static void should_dodge(string path)
+        {
+            foreach (string item in api.lobby_players)
+            {
+                StreamReader sr = new StreamReader(path);
+                string list = sr.ReadToEnd();
+                string[] lines = list.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+                IEnumerable<string> results = lines.Where(l => l.StartsWith(item));
+                string result = lines.SingleOrDefault(l => l.StartsWith(item));
+                string[] textLines = File.ReadAllLines(path);
+                ///Check blocked players if there is someone of these in lobby
+                if(api.get_blocked_players() != null)
+                {
+                    if (api.get_blocked_players().Contains(item))
+                    {
+                        ///Add player to "blacklist in session" list (support for multiple blacklisted players in one lobby)
+                        blacklisted_in_session.Add(new Tuple<string, string>(item, "PLAYER FROM BLOCKLIST"));
+                        globals.should_dodge = true;
+                        break;
+                    }
+                }
+
+                foreach (string line in textLines.Where(l => l.StartsWith(item)))
+                {
+                    /// If someone from Champion select is from blacklist or in blocked players list.
+                    if (api.lobby_players.Contains(item))
+                    {
+                        string s = line.Replace(item + " - ", "");
+                        ///Add player to "blacklist in session" list (support for multiple blacklisted players in one lobby)
+                        blacklisted_in_session.Add(new Tuple<string, string>(item, s));
+                        globals.should_dodge = true;
+                    }
+                    else
+                    {
+                        globals.should_dodge = false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ lol-dodge-alert/globals.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lol_dodge_alert
+{
+    public class globals
+    {
+        /// <summary>
+        /// Set your file path here
+        /// </summary>
+        public static string path = @"list.txt";
+
+        /// <summary>
+        /// General variables (do not modify)
+        /// </summary>
+        public static bool should_dodge;
+    }
+}
